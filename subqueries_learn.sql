f161ab8728ef8f6c15c90d8ef5ade4642a55f5e2
CynRees <chepkorir.cynthia04@gmail.com>
2023-05-03 16:58:10 +0300

    NEW QUERIES

--- subqueries_learn.sql
+++ subqueries_learn.sql
@@ -12,3 +12,12 @@ on f.`Grand Total` > avg_grand.grand;
 Select distinct ID from sales_invoice_datasets1;
 
 SELECT * FROM sales_invoice_datasets1;
+SELECT invoice_date,sum(`Grand Total`) FROM sales_invoice_datasets1 GROUP BY invoice_date; 
+
+SELECT year(invoice_date) AS Years, week(invoice_date) AS week_start_date, SUM(`Grand Total`) AS total_sales
+FROM sales_invoice_datasets1 GROUP BY week_start_date, Years ORDER BY week_start_date;
+SELECT DISTINCT `Customer` FROM sales_invoice_datasets1;
+SELECT `Customer`,SUM (`Grand Total`) FROM sales_invoice_datasets1 GROUP BY `Customer` ORDER BY (SUM(`Grand Total`))DESC;
+SELECT `Customer`, SUM(`Grand Total`) AS total_grand
+FROM sales_invoice_datasets1
+GROUP BY `Customer`;

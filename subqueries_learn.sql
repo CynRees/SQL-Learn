77996b4d9582f73ab1865e528f15d7bfcade25a8
CynRees <chepkorir.cynthia04@gmail.com>
2023-05-03 13:47:57 +0300

    new queries

--- subqueries_learn.sql
+++ subqueries_learn.sql
@@ -1,5 +1,4 @@
 SELECT * FROM rookery.sales_invoice_datasets1;
 USE rookery;
 SELECT COUNT(*) AS NUMBERS FROM sales_invoice_datasets1;
-SELECT DISTINCT ID, POS profile
-FROM sales_invoice_datasets1;
+SELECT DISTINCT `ID`FROM sales_invoice_datasets1;

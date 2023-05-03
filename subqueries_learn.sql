abd4d018799bface39f6509dd62ea504bde45ef6
CynRees <chepkorir.cynthia04@gmail.com>
2023-05-03 13:51:22 +0300

    Sales

--- subqueries_learn.sql
+++ subqueries_learn.sql
@@ -2,3 +2,11 @@ SELECT * FROM rookery.sales_invoice_datasets1;
 USE rookery;
 SELECT COUNT(*) AS NUMBERS FROM sales_invoice_datasets1;
 SELECT DISTINCT `ID`FROM sales_invoice_datasets1;
+
+SELECT * FROM rookery.sales_invoice_datasets1;
+SELECT avg(`Grand Total`) FROM sales_invoice_datasets1;
+SELECT *
+FROM sales_invoice_datasets1 f
+join (SELECT avg(`Grand Total`) grand FROM sales_invoice_datasets1) avg_grand
+on f.`Grand Total` > avg_grand.grand;
+Select distinct ID from sales_invoice_datasets1;

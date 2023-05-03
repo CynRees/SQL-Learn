ab21e77527e756340a7b7a37da5318eeb3feaf5f
CynRees <chepkorir.cynthia04@gmail.com>
2023-05-03 13:40:06 +0300

    first changes

---
+++ subqueries_learn.sql
@@ -0,0 +1,5 @@
+SELECT * FROM rookery.sales_invoice_datasets1;
+USE rookery;
+SELECT COUNT(*) AS NUMBERS FROM sales_invoice_datasets1;
+SELECT DISTINCT ID, POS profile
+FROM sales_invoice_datasets1;
